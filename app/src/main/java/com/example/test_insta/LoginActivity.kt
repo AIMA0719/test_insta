66464a37a13545c348e0ceac321443e9a9c87469
jay_choi <dkwkrhrh07@naver.com>
2022-07-30 17:11:27 +0900

    3강

--- app/src/main/java/com/example/test_insta/LoginActivity.kt
+++ app/src/main/java/com/example/test_insta/LoginActivity.kt
@@ -6,10 +6,13 @@ import android.os.Bundle
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.example.test_insta.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
 
@@ -29,16 +32,47 @@ class LoginActivity : AppCompatActivity() {
         binding.emailLogin.setOnClickListener {
             signInAndSignup()
         }
+
+        binding.googleLogin.setOnClickListener{
+            googleLogin()
+        }
         var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken("387901556124-1emndo1u166hv7gvk2gk7fhe0otl4rss.apps.googleusercontent.com")
             .requestEmail()
             .build()
         googleSignInClient = GoogleSignIn.getClient(this,gso)
     }
-//    fun googleLogin(){
-//        var signInIntent = googleSignInClient?.signInIntent
-//        startActivityForResult(signInIntent,Google_LOGIN_CODE)
-//    }
+    fun googleLogin(){
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent,Google_LOGIN_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == Google_LOGIN_CODE){
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result != null) {
+                if ( result.isSuccess){
+                    var account = result.signInAccount
+                    firebaseAuthWithGoogle(account)
+                }
+            }
+        }
+    }
+
+    fun firebaseAuthWithGoogle(account : GoogleSignInAccount?){
+        var creadential = GoogleAuthProvider.getCredential(account?.idToken,null)
+        auth?.signInWithCredential(creadential)
+            ?.addOnCompleteListener {
+                    task ->
+                if(task.isSuccessful){
+                    moveMainPage(task.result?.user)
+                } else{
+                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_SHORT).show()
+                }
+            }
+
+    }
 
 
     fun signInAndSignup(){
